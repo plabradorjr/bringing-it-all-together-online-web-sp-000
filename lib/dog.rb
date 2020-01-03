d726ca61821133118ace764d0a2e400d3ffbee9d
Pancho Labrador <plabradorjr@gmail.com>
2020-01-03 10:54:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,4 +20,11 @@ class Dog
       DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF NOT EXISTS dogs
+      SQL
+      DB[:conn].execute(sql)
+  end
+
 end
